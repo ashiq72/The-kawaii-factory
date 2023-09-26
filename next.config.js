1ec52f0f57d5481814d54338c49a51917032adad
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-09-26 16:25:32 +0600

    Projects initial

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-    ],
-    domains: ["i.postimg.cc", "static.nike.com", "www.aarong.com"],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
