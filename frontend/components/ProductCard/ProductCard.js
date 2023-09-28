04f603b712f489fea0306aa64a204a461026526f
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-09-28 10:17:35 +0600

    Product card updaded again

--- frontend/components/ProductCard/ProductCard.js
+++ frontend/components/ProductCard/ProductCard.js
@@ -1,152 +1,173 @@
 "use client";
 // import { getDiscountPricePercentage } from "@/utils/helper";
-import { Typography, Button } from "@material-tailwind/react";
+import { Typography, Button, Tooltip } from "@material-tailwind/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { AiOutlineHeart, AiOutlineEye } from "react-icons/ai";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 export function ProductCard({ product }) {
-  const [hoverImg, setHoverImg] = useState(false);
-  console.log(hoverImg);
   const { name, price, image, slug, thumbnail, orginalPrice } = product;
-  const [hoverCartBtn, setHoverCartBtn] = useState(false);
+  const [hoverCart, setHoverCart] = useState(false);
 
   return (
-    <div className="w-80 h-[520px] p-2 m-4 border-2">
-      {/* <div
-        className="h-[300px] bg-center overflow-hidden shadow-sm"
-        onMouseEnter={() => setHoverImg(true)}
-        onMouseLeave={() => setHoverImg(false)}
+    <div className="w-80 h-[520px]  border-2 border-gray-50 rounded-md">
+      {/* --------------------------------------------------
+       Start Image part : Background image, front image, add to cart button, wishlist, quick view  
+       ---------------------------------------------------*/}
+      <div
+        className="h-[400px] max-h-full bg-center overflow-hidden shadow-sm relative"
+        onMouseEnter={() => setHoverCart(!hoverCart)}
+        onMouseLeave={() => setHoverCart(!hoverCart)}
       >
-        {hoverImg ? (
-          <>
-            {image.slice(1, 2).map((img, index) => (
-              <div key={index}>
-                <Image
-                  src={img}
-                  width={300}
-                  height={300}
-                  alt="card-image"
-                  className="h-full w-full object-cover relative duration-1000 ease-in-out hover:origin-center hover:scale-125 "
-                />
-                <h1 className="absolute top-5 right-5 z-50">Hello</h1>
-              </div>
-            ))}
-          </>
-        ) : (
-          <>
-            {image.slice(0, 1).map((img, index) => (
-              <div key={index}>
-                <Image
-                  src={img}
-                  width={300}
-                  height={300}
-                  alt="card-image"
-                  className="h-full w-full object-cover   "
-                />
-              </div>
-            ))}
-          </>
-        )}
-      </div> */}
-      <div className="h-[300px] bg-center overflow-hidden shadow-sm relative  ">
         <>
+          {/*---------------- Front Image ----------------*/}
           {image.slice(1, 2).map((img, index) => (
             <>
               <div
                 key={index}
-                className="h-full w-full absolute object-cover opacity-0 z-40 ease-in-out duration-1000 hover:scale-105 hover:opacity-100  "
+                className={`h-full w-full absolute object-cover  z-40  ${
+                  hoverCart
+                    ? "scale-105 hover:opacity-100 ease-in-out duration-1000"
+                    : "opacity-0 ease-in-out duration-1000"
+                }  `}
               >
                 <Image
                   src={img}
-                  width={300}
+                  width={340}
                   height={300}
                   alt="card-image"
                   className="object-cover"
-                  onMouseEnter={() => setHoverCartBtn(!hoverCartBtn)}
-                  onMouseLeave={() => setHoverCartBtn(!hoverCartBtn)}
                 />
               </div>
             </>
           ))}
 
-          <h1
-            className={`absolute top-5 duration-500 z-40 text-2xl flex flex-col gap-2 ${
-              hoverCartBtn ? "right-4" : "-right-10"
-            }`}
-          >
-            <AiOutlineHeart />
-            <AiOutlineEye />
-          </h1>
-
+          {/*------------ Background Image --------------- */}
           {image.slice(0, 1).map((img, index) => (
             <>
               <div
                 key={index}
-                className="h-full w-full absolute z-20 object-cover opacity-100 hover:opacity-0"
+                className={`h-full w-full absolute z-20 object-cover ${
+                  hoverCart ? " hover:opacity-0" : " opacity-100"
+                }  `}
               >
                 <Image
                   src={img}
-                  width={300}
+                  width={340}
                   height={300}
                   alt="card-image"
-                  className=""
+                  className="object-cover"
                 />
               </div>
             </>
           ))}
+          {/* Start Button & Wishlist & Add to cart  */}
+          <h1
+            className={`absolute top-5 duration-500 z-40 text-2xl flex flex-col gap-2 ${
+              hoverCart ? "right-2" : "-right-20"
+            }`}
+          >
+            <div className="flex gap-3">
+              <Tooltip
+                className="z-40 p-1 px-2"
+                content="Add to wishlist"
+                placement="left"
+                animate={{
+                  mount: { scale: 1, y: 0 },
+                  unmount: { scale: 0, y: 25 },
+                }}
+              >
+                <div className="bg-white rounded-full p-1 border-2 ease-in-out duration-100 border-gray-50 hover:border-2 hover:border-gray-900">
+                  <AiOutlineHeart />
+                </div>
+              </Tooltip>
+            </div>
+            <div className="flex gap-3">
+              <Tooltip
+                className="z-40 py-1 px-2"
+                content="Quick view"
+                placement="left"
+                animate={{
+                  mount: { scale: 1, y: 0 },
+                  unmount: { scale: 0, y: 25 },
+                }}
+              >
+                <div className="bg-white rounded-full p-1 border-2 ease-in-out duration-100 border-gray-50 hover:border-2 hover:border-gray-900">
+                  <AiOutlineEye />
+                </div>
+              </Tooltip>
+            </div>
+          </h1>
+          <div
+            className={`absolute origin-center  duration-500 z-40 translate-x-1/2  gap-2 bg-white rounded-md  ${
+              hoverCart ? "bottom-2" : "-bottom-20"
+            }`}
+          >
+            <div class="relative flex  flex-col items-center justify-center overflow-hidden bg-gray-50 z-10 rounded-md ">
+              <button class="relative w-full h-full bg-transparent py-2 px-6 font-medium uppercase text-gray-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-gray-800 before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 rounded-md ">
+                Add to cart
+              </button>
+            </div>
+          </div>
+          {/* End Button & Wishlist & Add to cart  */}
         </>
       </div>
-      {/*------------ Name ----------- */}
-      <div className="font-[500] text-[16px] pt-4">
-        {name.length > 32 ? (
-          <p>
-            {name.slice(0, 32)}
-            <span>...</span>
-          </p>
-        ) : (
-          { name }
-        )}
+      {/* --------------------------------------------------
+       End Image part : Background image, front image, add to cart button, wishlist, quick view  
+       ---------------------------------------------------*/}
+      {/* --------------------------------------------------
+       Start Name, review star, price
+       ---------------------------------------------------*/}
+      <div className="px-1">
+        {/*------------ Name ----------- */}
+        <div className="font-[500] text-[16px] pt-5 hover:text-green-500">
+          {name.length > 32 ? (
+            <p>
+              {name.slice(0, 32)}
+              <span>...</span>
+            </p>
+          ) : (
+            { name }
+          )}
+        </div>
+        {/* -----------Reviews ------------ */}
+        <div className="flex gap-1 text-xl py-2">
+          <span className="text-yellow-600">
+            <AiFillStar />
+          </span>
+          <span className="text-yellow-600">
+            <AiFillStar />
+          </span>
+          <span className="text-yellow-600">
+            <AiFillStar />
+          </span>
+          <span className="text-yellow-600">
+            <AiFillStar />
+          </span>
+          <span className="text-gray-600">
+            <AiOutlineStar />
+          </span>
+        </div>
+        {/*------------ Price ----------- */}
+        <div className="font-normal text-base py-1 flex px-1 gap-3">
+          &#2547; {price}
+          {/* {orginalPrice && ( */}
+          <>
+            <div className="text-base text-red-700 font-medium line-through">
+              &#2547; 1200
+            </div>
+            <div className="ml-auto text-base font-medium text-green-500">
+              <p>12 % Off</p>
+            </div>
+          </>
+          {/* )} */}
+        </div>
       </div>
-      {/*------------ Price ----------- */}
-      <div className="font-normal text-base">
-        &#2547; {price}
-        {/* {orginalPrice && (
-              <>
-                <div className="text-base  font-medium line-through">
-                  &#8377;{orginalPrice}
-                </div>
-                <div className="ml-auto text-base font-medium text-green-500">
-                  <p>
-                   
-                    12 % Off
-                  </p>
-                </div>
-              </>
-            )} */}
-      </div>
-
-      <Typography
-        variant="small"
-        color="gray"
-        className="font-normal opacity-75"
-      >
-        With plenty of talk and listen time, voice-activated Siri access, and an
-        available wireless charging case.
-      </Typography>
-
-      <div className="pt-0">
-        <Link href="">
-          <Button
-            ripple={false}
-            fullWidth={true}
-            className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
-          >
-            Quick View
-          </Button>
-        </Link>
-      </div>
+      {/* --------------------------------------------------
+       End Name, review star, price
+       ---------------------------------------------------*/}
     </div>
   );
 }
