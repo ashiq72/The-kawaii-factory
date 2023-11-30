67225e03af60d20bf31baa6ff724786dbc8bcf3a
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-11-30 12:27:42 +0600

    Product details view issuse fixed

--- app/product-view/[id]/page.js
+++ app/product-view/[id]/page.js
@@ -1,9 +1,12 @@
 import ProductDetails from "@/app/components/ProductDetails/ProductsDetails";
 
 const ProductView = async ({ params }) => {
-  const res = await fetch(`http://localhost:5000/api/v1/product/${params.id}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `https://server-kawaiisss.vercel.app/api/v1/product/${params.id}`,
+    {
+      cache: "no-store",
+    }
+  );
   const product = await res.json();
 
   return (
