6041c76ff3b6c86b87cc95d7f89a9737d30911fc
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-12-12 18:33:18 +0600

    Top category updated

--- app/components/Home/TopCategory/TopCategory.js
+++ app/components/Home/TopCategory/TopCategory.js
@@ -1,8 +1,14 @@
+import { menuCategory } from "@/store/features/categoryFilterSlice/categoryFilterSlice";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { GiDress } from "react-icons/gi";
+import { PiWatchThin } from "react-icons/pi";
+import { PiTShirtThin } from "react-icons/pi";
+import { PiHandbagSimpleThin } from "react-icons/pi";
+import { useDispatch } from "react-redux";
 
 export const TopCategory = () => {
+  const dispatch = useDispatch();
   return (
     <div className="py-20  w-11/12 mx-auto">
       <div className="text-center">
@@ -10,7 +16,13 @@ export const TopCategory = () => {
       </div>
       <div className="grid grid-cols-5 gap-4 pt-8">
         {/* 1- Tops category  */}
-        <div className="bg-pink-100 rounded flex justify-between items-center px-8 py-5">
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("tops"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
           <span className="text-3xl text-gray-600">
             <Image
               alt=" "
@@ -20,10 +32,16 @@ export const TopCategory = () => {
             />
           </span>
 
-          <h3 className="text-xl font-normal text-gray-600">Tops</h3>
-        </div>
+          <h3 className="text-lg font-medium text-gray-600">Tops</h3>
+        </Link>
         {/* 2- Long Frock category  */}
-        <div className="bg-pink-100 rounded flex justify-between items-center px-8 py-5">
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("long frock"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
           <span className="text-3xl text-gray-600">
             <Image
               alt=" "
@@ -33,10 +51,16 @@ export const TopCategory = () => {
             />
           </span>
 
-          <h3 className="text-xl font-normal text-gray-600">Long Frock</h3>
-        </div>
+          <h3 className="text-lg font-medium text-gray-600"> Long Frock </h3>
+        </Link>
         {/* 3- Jeggings category  */}
-        <div className="bg-pink-100 rounded flex justify-between items-center px-8 py-5">
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("jeggings"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
           <span className="text-3xl text-gray-600">
             <Image
               alt=" "
@@ -46,10 +70,16 @@ export const TopCategory = () => {
             />
           </span>
 
-          <h3 className="text-xl font-normal text-gray-600">Jeggings</h3>
-        </div>
+          <h3 className="text-lg font-medium text-gray-600">Jeggings</h3>
+        </Link>
         {/* 4- Party Dress category  */}
-        <div className="bg-pink-100 rounded flex justify-between items-center px-8 py-5">
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("party dress"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
           <span className="text-3xl text-gray-600">
             <Image
               alt=" "
@@ -59,10 +89,16 @@ export const TopCategory = () => {
             />
           </span>
 
-          <h3 className="text-xl font-normal text-gray-600">Party Dress</h3>
-        </div>
+          <h3 className="text-lg font-medium text-gray-600">Party Dress</h3>
+        </Link>
         {/* 5- Skirt category  */}
-        <div className="bg-pink-100 rounded flex justify-between items-center px-8 py-5">
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("skirt"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
           <span className="text-3xl text-gray-600">
             <Image
               alt=" "
@@ -72,21 +108,111 @@ export const TopCategory = () => {
             />
           </span>
 
-          <h3 className="text-xl font-normal text-gray-600">Skirt</h3>
-        </div>
-        {/* 6- Skirt category  */}
-        <div className="bg-pink-100 rounded flex justify-between items-center px-8 py-5">
+          <h3 className="text-lg font-medium text-gray-600">Skirt</h3>
+        </Link>
+        {/* 6- Watch category  */}
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("shirts"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
+          <span className="text-5xl text-gray-600">
+            {/* <Image
+              alt=" "
+              src="https://i.postimg.cc/9XqH5bBH/ezgif-com-crop-3-removebg-preview.png"
+              width="40"
+              height="40"
+            /> */}
+            <PiTShirtThin />
+          </span>
+
+          <h3 className="text-lg font-medium text-gray-600">Women Shirts</h3>
+        </Link>
+        {/* 7- Kids Dress category  */}
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("dress"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
           <span className="text-3xl text-gray-600">
             <Image
               alt=" "
-              src="https://i.postimg.cc/7LznkTjY/ezgif-com-png-to-jpg-converted-removebg-preview.png"
+              src="https://i.postimg.cc/9XqH5bBH/ezgif-com-crop-3-removebg-preview.png"
               width="40"
               height="40"
             />
           </span>
 
-          <h3 className="text-xl font-normal text-gray-600">Tops</h3>
-        </div>
+          <h3 className="text-lg font-medium text-gray-600">Kids Dress</h3>
+        </Link>
+
+        {/* 8- Watch category  */}
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("watch"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
+          <span className="text-5xl text-gray-600">
+            {/* <Image
+              alt=" "
+              src="https://i.postimg.cc/9XqH5bBH/ezgif-com-crop-3-removebg-preview.png"
+              width="40"
+              height="40"
+            /> */}
+            <PiWatchThin />
+          </span>
+
+          <h3 className="text-lg font-medium text-gray-600">Watch</h3>
+        </Link>
+
+        {/* 9- Shoes category  */}
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("shoes"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
+          <span className="text-3xl text-gray-600">
+            <Image
+              alt=" "
+              src="https://i.postimg.cc/8zVs2jQN/shoes-removebg-preview.png"
+              width="50"
+              height="50"
+            />
+          </span>
+
+          <h3 className="text-lg font-medium text-gray-600">Shoes</h3>
+        </Link>
+
+        {/* 10- Bags category  */}
+        <Link
+          href="/products"
+          onClick={() => {
+            dispatch(menuCategory("bags"));
+          }}
+          className="bg-pink-100 rounded flex justify-between items-center px-8 py-5 hover:shadow-xl duration-200 hover:underline cursor-pointer"
+        >
+          <span className="text-5xl text-gray-600">
+            {/* <Image
+              alt=" "
+              src="https://i.postimg.cc/9XqH5bBH/ezgif-com-crop-3-removebg-preview.png"
+              width="40"
+              height="40"
+            /> */}
+            <PiHandbagSimpleThin />
+          </span>
+
+          <h3 className="text-lg font-medium text-gray-600  duration-500">
+            Bags
+          </h3>
+        </Link>
       </div>
     </div>
   );
