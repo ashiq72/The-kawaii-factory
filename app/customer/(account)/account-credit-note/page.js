934c67e9478ce758788dd007f6f300bd9ce23f7e
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-12-27 18:57:58 +0600

    Account other page added

--- app/customer/(account)/account-credit-note/page.js
+++ app/customer/(account)/account-credit-note/page.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function AccountCreditNote() {
-  return <div>AccountCreditNote</div>;
+  return (
+    <div>
+      <div>
+        <h1 className=" font-bold text-lg">CREDIT NOTE</h1>
+      </div>
+      <div className=" bg-white mt-2 rounded ">
+        <div className="font-semibold border-b-2 px-4 py-3 border-gray-100">
+          Summary
+        </div>
+        <div className="px-4 py-8">
+          <p className="font-sans text-base">Your Balance Is Tk 0.00</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AccountCreditNote;
