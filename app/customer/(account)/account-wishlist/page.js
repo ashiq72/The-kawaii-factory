2249dda75fe84f421e6d11bbbc4998bef013fa3a
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-12-27 18:27:09 +0600

    Accoun  wishlist added

--- app/customer/(account)/account-wishlist/page.js
+++ app/customer/(account)/account-wishlist/page.js
@@ -1,7 +1,20 @@
 import React from "react";
+import AccountWishlists from "./account-wishlist";
 
 function AccountWishlist() {
-  return <div>AccountWishlist</div>;
+  return (
+    <div>
+      <div>
+        <h1 className=" font-bold text-lg">WISHLIST</h1>
+        <p className="text-sm text-gray-500 font-sans">
+          All your saved items in one place.
+        </p>
+      </div>
+      <div>
+        <AccountWishlists />
+      </div>
+    </div>
+  );
 }
 
 export default AccountWishlist;
